a2d658c15f505d53fed2be2aa0051a2e22825eef
naughtyfox <mail.for.milo@gmail.com>
2019-09-16 18:12:32 +0300

    build and deploy fixed under ubuntu 16.04

--- linuxdeploy_helper.sh
+++ linuxdeploy_helper.sh
@@ -30,16 +30,17 @@ if [ -z "$QTXML_DIR" ]; then
 fi
 
 # Copy dependencies
-EXCLUDE='libstdc++|libgcc_s.so|libc.so|libpthread'
+EXCLUDE='libstdc++|libgcc_s.so|libc.so|libpthread|libdl|libGL|libm|libz|libglib|libgthread|librt'
 INCLUDE='libunbound'
 cp -rv $QT_DIR/qml $TARGET || exit
 cp -rv $QT_DIR/plugins $TARGET || exit
 mkdir -p $TARGET/libs || exit
 #ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep $INCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
-#ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
-#ldd $TARGET/plugins/platforms/libqxcb.so| grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
-#cp -v $QTXML_DIR/libQt5XmlPatterns.so.5 $TARGET/libs || exit
-
+ldd $TARGET/$GUI_EXEC | grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
+ldd $TARGET/plugins/platforms/libqxcb.so| grep "=> /" | awk '{print $3}' | grep -Ev $EXCLUDE | xargs -I '{}' cp -v '{}' $TARGET/libs || exit
+cp -v $QTXML_DIR/libQt5XmlPatterns.so.5 $TARGET/libs || exit
+# cp -v $QTXML_DIR/libQt5QuickTemplates2.so.5 $TARGET/libs || exit
+cp -v $QTXML_DIR/libQt5*.so.5 $TARGET/libs || exit
 # Create start script
 cat > $TARGET/start-gui.sh <<EOL
 #!/bin/bash
